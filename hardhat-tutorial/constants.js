17d06dd4204c523299c29e666a2907e1103d00fa
Marcus Torry <91373494+mtorry225@users.noreply.github.com>
2022-02-05 23:33:42 -0600

    CryptoDevs DAO

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xC36678aA7947760E544478B3839E8117b5e668da";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
